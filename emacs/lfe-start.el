40aac8705606d58b5bd8fd0416ebc8e8ed8f4266
Robert Virding <rvirding@gmail.com>
2013-02-24 01:00:38 +0100

    Convert to use Apache License, Version 2.0

--- emacs/lfe-start.el
+++ emacs/lfe-start.el
@@ -1,9 +1,24 @@
+;; Copyright (c) 2012-2013 Robert Virding
+;;
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+;;
+;;     http://www.apache.org/licenses/LICENSE-2.0
+;;
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
+
 ;;; lfe-start.el - Initialise the LFE mode package.
 ;;; Author: Robert Virding
 
 ;; Declare autoload functions in lfe-mode.el and inferior-lfe.el.
 (autoload 'lfe-mode "lfe-mode" "Major mode for editing LFE code." t)
-(autoload 'inferior-lfe-mode "inferior-lfe" "Major mode for interacting with an inferior LFE process." t)
+(autoload 'inferior-lfe-mode "inferior-lfe"
+  "Major mode for interacting with an inferior LFE process." t)
 (autoload 'inferior-lfe "inferior-lfe" "Run an LFE process." t)
 (autoload 'run-lfe "inferior-lfe" "Run an LFE process." t)
 
