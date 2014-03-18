0623aa5e851fe09d10919d3609ef12ea5fca07ff
Robert Virding <rvirding@gmail.com>
2014-03-18 11:59:28 +0100

    Add special handling of pre-R16 packages in lfe_trans

--- src/lfe_trans.erl
+++ src/lfe_trans.erl
@@ -129,6 +129,8 @@ from_expr({record_field,_,E,R,{atom,_,F}}, Vt0, St0) ->	%We KNOW!
     RF = list_to_atom(atom_to_list(R) ++ "-" ++ atom_to_list(F)),
     {Le,Vt1,St1} = from_expr(E, Vt0, St0),
     {[RF,Le],Vt1,St1};
+from_expr({record_field,_,_,_}=M, Vt, St) ->		%Pre R16 packages
+    from_package_module(M, Vt, St);
 %% Function calls.
 from_expr({call,_,{remote,_,M,F},As}, Vt0, St0) ->	%Remote function call
     {Lm,Vt1,St1} = from_expr(M, Vt0, St0),
@@ -173,6 +175,16 @@ from_expr({op,_,Op,L,R}, Vt0, St0) ->
     {Ll,Vt1,St1} = from_expr(L, Vt0, St0),
     {Lr,Vt2,St2} = from_expr(R, Vt1, St1),
     {[Op,Ll,Lr],Vt2,St2}.
+
+%% from_package_module(Module, VarTable, State) -> {Module,VarTable,State}.
+%%  We must handle the special case where in pre-R16 you could have
+%%  packages with a dotted module path. It used a special record_field
+%%  tuple. This does not work in R16 and later!
+
+from_package_module({record_field,_,_,_}=M, Vt, St) ->
+    Segs = erl_parse:package_segments(M),
+    A = list_to_atom(packages:concat(Segs)),
+    {?Q(A),Vt,St}.
 
 from_maybe(_, []) -> [];
 from_maybe(Tag, Es) -> [[Tag|Es]].
