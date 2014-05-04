fcc8b4b687b2de478c8ceac9c776bbddedd39586
Duncan McGreggor <duncan@cogitat.io>
2014-05-04 08:59:25 -0700

    Updated version numbers to release values.

--- package.exs
+++ package.exs
@@ -1,7 +1,7 @@
 Expm.Package.new(name: "lfe",
                  description: "Lisp Flavored Erlang",
-                 version: "0.6.2",
-                 keywords: ["LFE", "Lisp"],
+                 version: "0.8.0",
+                 keywords: ["LFE", "Lisp", "Languages", "BEAM", "Erlang VM"],
                  maintainers: [[name: "Robert Virding",
                                 email: "rvirding@gmail.com"]],
                  repositories: [[github: "rvirding/lfe"]])
