cc37186beba865fc0919550fe385a337472a0134
Robert Virding <rvirding@gmail.com>
2014-10-22 01:15:59 +0200

    Fix errors in documentation and add defining functions and macros

--- package.exs
+++ package.exs
@@ -1,6 +1,6 @@
 Expm.Package.new(name: "lfe",
                  description: "Lisp Flavored Erlang",
-                 version: "0.9.0-dev",
+                 version: "0.9.0",
                  keywords: ["LFE", "Lisp", "Languages", "BEAM", "Erlang VM"],
                  maintainers: [[name: "Robert Virding",
                                 email: "rvirding@gmail.com"]],
