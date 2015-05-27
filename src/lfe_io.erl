47dbf0d25aa0ae950ab7279b95340ddfdd02aef1
Robert Virding <rvirding@gmail.com>
2015-05-28 00:49:07 +0200

    Improve error returns from lfe_io and handling in lfe_parse
    Also take the chance to fix to indentation. The interface to
    lfe_io:read still unchanged.

--- src/lfe_io.erl
+++ src/lfe_io.erl
@@ -52,13 +52,13 @@ parse_file(Name) ->
 
 parse_file1([_|_]=Ts0, Pc0, Ss) ->
     case lfe_parse:sexpr(Pc0, Ts0) of
-    {ok,L,S,Ts1} -> parse_file1(Ts1, [], [{S,L}|Ss]);
-    {more,Pc1} ->
-        %% Need more tokens but there are none, so call again to
-        %% generate an error message.
-        {error,E,_} = lfe_parse:sexpr(Pc1, {eof,99999}),
-        {error,E};
-    {error,E,_} -> {error,E}
+        {ok,L,S,Ts1} -> parse_file1(Ts1, [], [{S,L}|Ss]);
+        {more,Pc1} ->
+            %% Need more tokens but there are none, so call again to
+            %% generate an error message.
+            {error,E,_} = lfe_parse:sexpr(Pc1, {eof,99999}),
+            {error,E};
+        {error,E,_} -> {error,E}
     end;
 parse_file1([], _, Ss) -> {ok,reverse(Ss)}.
 
@@ -95,17 +95,24 @@ with_token_file(Name, Do) ->
         {error,Error} -> {error,{none,file,Error}}
     end.
 
-%% read([IoDevice]) -> {ok,Sexpr} | {error,Error}.
+%% read() -> {ok,Sexpr} | {error,Error}.
+%% read(Prompt) -> {ok,Sexpr} | {error,Error}.
+%% read(IoDevice, Prompt) -> {ok,Sexpr} | {error,Error}.
 %%  A very simple read function. Line oriented and cannot handle
 %%  tokens over line-breaks but can handle multiple lines. Anything
 %%  remaining on last line after a sexpr is lost. Signal errors.
 
+%% read() -> read(standard_io, '').
+%% read(Prompt) -> read(standard_io, Prompt).
+%% read(Io, Prompt) ->
+%%     scan_and_parse(Io, Prompt, [], 1).
 read() -> read(standard_io).
 read(Io) ->
-    scan_and_parse(Io, [], 1).
+    scan_and_parse(Io, '', [], 1).
 
-scan_and_parse(Io, Pc0, L) ->
-    case io:get_line(Io, '') of
+scan_and_parse(Io, Prompt, Pc0, L) ->
+    case io:get_line(Io, Prompt) of
+        {error,E} -> {error,{L,lfe_parse,E}};
         eof ->
             %% No more so must take what we have.
             case lfe_parse:sexpr(Pc0, {eof,L}) of
@@ -116,11 +123,11 @@ scan_and_parse(Io, Pc0, L) ->
             case lfe_scan:string(Cs, L) of
                 {ok,[],_} ->
                     %% Empty line (token free) just go on.
-                    scan_and_parse(Io, Pc0, L+1);
+                    scan_and_parse(Io, Prompt, Pc0, L+1);
                 {ok,Ts,_} ->
                     case lfe_parse:sexpr(Pc0, Ts) of
                         {ok,_,S,_} -> {ok,S};
-                        {more,Pc1} -> scan_and_parse(Io, Pc1, L+1);
+                        {more,Pc1} -> scan_and_parse(Io, Prompt, Pc1, L+1);
                         {error,E,_} -> {error,E}
                     end;
                 {error,E,_} -> {error,E}
