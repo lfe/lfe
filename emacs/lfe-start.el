9b9307f271cf498df02e3f933862ef78ca0f3ac3
Robert Virding <rvirding@gmail.com>
2015-11-09 02:37:16 +0100

    Split lfe-mode into multiple files, some improvements of indentation
    This is starting the work of improving LFE indentation. It now uses
    local LFE functions and variable values. Also can now compile emacs
    .el files with 'make emacs'.

--- emacs/lfe-start.el
+++ emacs/lfe-start.el
@@ -17,6 +17,7 @@
 
 ;; Declare autoload functions in lfe-mode.el and inferior-lfe.el.
 (autoload 'lfe-mode "lfe-mode" "Major mode for editing LFE code." t)
+(autoload 'lfe-indent-function "lfe-indent" "Indent LFE." t)
 (autoload 'inferior-lfe-mode "inferior-lfe"
   "Major mode for interacting with an inferior LFE process." t)
 (autoload 'inferior-lfe "inferior-lfe" "Run an LFE process." t)
