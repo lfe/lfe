d102f6412992a37e01666cd57bdb5ecd1828b171
arpunk <arpunk@cryptolab.net>
2015-12-28 19:09:34 -0500

    Deprecate old PM

--- package.exs
+++
@@ -1,7 +0,0 @@
-Expm.Package.new(name: "lfe",
-                 description: "Lisp Flavored Erlang",
-                 version: "0.10.0-dev",
-                 keywords: ["LFE", "Lisp", "Languages", "BEAM", "Erlang VM"],
-                 maintainers: [[name: "Robert Virding",
-                                email: "rvirding@gmail.com"]],
-                 repositories: [[github: "rvirding/lfe"]])
