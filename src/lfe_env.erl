d19bf474b8b4a8f397581e0ba8c68f59dbb2eb40
Robert Virding <rvirding@gmail.com>
2016-01-23 17:37:24 +0100

    Fold over variables, functions and macros
    Also took the chance to use maps when we have them.

--- src/lfe_env.erl
+++ src/lfe_env.erl
@@ -1,4 +1,4 @@
-%% Copyright (c) 2008-2014 Robert Virding
+%% Copyright (c) 2008-2016 Robert Virding
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -19,7 +19,8 @@
 -module(lfe_env).
 
 -export([new/0,add_env/2,
-         get_vars/1,clr_vars/1,set_vars/2,get_funs/1,clr_funs/1,set_funs/2,
+         get_vars/1,clr_vars/1,set_vars/2,fold_vars/3,
+         get_funs/1,clr_funs/1,set_funs/2,fold_funs/3,fold_macros/3,
          add_vbinding/3,add_vbindings/2,is_vbound/2,get_vbinding/2,
          fetch_vbinding/2,del_vbinding/2,
          add_fbinding/4,add_fbindings/2,
@@ -31,8 +32,33 @@
 -import(lfe_lib, [is_bif/2,is_lfe_bif/2,is_erl_bif/2,is_guard_bif/2]).
 -import(lists, [reverse/1,reverse/2,map/2,foldl/3,dropwhile/2]).
 
+%% Define access macros depending on whether we have maps.
+-ifdef(HAS_MAPS).
+-define(NEW(), #{}).
+-define(IS_KEY(K, D), maps:is_key(K, D)).
+-define(GET(K, D), maps:get(K, D)).
+-define(FIND(K, D), maps:find(K, D)).
+-define(PUT(K, V, D), (D)#{K => V}).
+-define(ERASE(K, D), maps:remove(K, D)).
+-define(FOLD(F, A, D), maps:fold(F, A, D)).
+-define(UPDATE(K, UPD, DEF, D),                 %This is slightly complex
+        case maps:find(K, D) of
+            {ok,___V} -> D#{K := UPD(___V)};
+            error -> D#{K => DEF}
+        end).
+-else.
+-define(NEW(), orddict:new()).
+-define(IS_KEY(K, D), orddict:is_key(K, D)).
+-define(GET(K, D), orddict:fetch(K, D)).
+-define(FIND(K, D), orddict:find(K, D)).
+-define(PUT(K, V, D), orddict:store(K, V, D)).
+-define(ERASE(K, D), orddict:erase(K, D)).
+-define(FOLD(F, A, D), orddict:fold(F, A, D)).
+-define(UPDATE(K, UPD, DEF, D), orddict:update(K, UPD, DEF, D)).
+-endif.
+
 %% The environment structure.
--record(env, {vars=[],funs=[]}).
+-record(env, {vars=null,funs=null}).
 
 %% -compile([export_all]).
 
@@ -41,9 +67,12 @@
 %% get_vars(Env) -> Vars.
 %% clr_vars(Env) -> Env.
 %% set_vars(Vars, Env) -> Env.
+%% fold_vars(Fun, Acc, Env) -> Acc.
 %% get_funs(Env) -> Funs.
 %% clr_funs(Env) -> Env.
 %% set_funs(Funs, Env) -> Env.
+%% fold_funs(Fun, Acc, Env) -> Acc.
+%% fold_macros(Fun, Acc, Env) -> Acc.
 %% add_vbinding(Name, Val, Env) -> Env.
 %% add_vbindings([{Name,Val}], Env) -> Env.
 %% is_vbound(Symb, Env) -> bool().
@@ -79,44 +108,69 @@
 %%  is no longer a legal guard bif but must be explicitly called with
 %%  module erlang.
 
-new() -> #env{vars=orddict:new(),funs=orddict:new()}. 
+new() -> #env{vars=?NEW(),funs=?NEW()}. 
 
+-ifdef(HAS_MAPS).
+add_env(#env{vars=Vs1,funs=Fs1}, #env{vars=Vs2,funs=Fs2}) ->
+    #env{vars=maps:merge(Vs2, Vs1),             %Always take left env
+         funs=maps:merge(Fs2, Fs1)}.
+-else.
 add_env(#env{vars=Vs1,funs=Fs1}, #env{vars=Vs2,funs=Fs2}) ->
     Merge = fun (_, V1, _) -> V1 end,           %Always take left env
     #env{vars=orddict:merge(Merge, Vs1, Vs2),
          funs=orddict:merge(Merge, Fs1, Fs2)}.
+-endif.
 
 get_vars(Env) -> Env#env.vars.
-clr_vars(Env) -> Env#env{vars=orddict:new()}.
+clr_vars(Env) -> Env#env{vars=?NEW()}.
 set_vars(Vars, Env) -> Env#env{vars=Vars}.
+fold_vars(Fun, Acc, Env) ->
+    ?FOLD(Fun, Acc, Env#env.vars).
 
 get_funs(Env) -> Env#env.funs.
-clr_funs(Env) -> Env#env{funs=orddict:new()}.
+clr_funs(Env) -> Env#env{funs=?NEW()}.
 set_funs(Funs, Env) -> Env#env{funs=Funs}.
+
+fold_funs(Fun, Acc, Env) ->
+    Ofun = fun (F, {function,Fs}, Ac) ->        %Function
+                   Ffun = fun ({Ar,Def}, A) ->
+                                  Fun(F, Ar, Def, A)
+                          end,
+                   lists:foldl(Ffun, Ac, Fs);
+               (_, _, A) -> A                   %Macro
+           end,
+    ?FOLD(Ofun, Acc, Env#env.funs).
+
+fold_macros(Fun, Acc, Env) ->
+    Ofun = fun (F, {macro,Def}, A) ->           %Macro
+                   Fun(F, Def, A);
+               (_, _, A) -> A                   %Function
+           end,
+    ?FOLD(Ofun, Acc, Env#env.funs).
 
 %% Variables.
 
 add_vbinding(N, V, #env{vars=Vs}=Env) ->
-    Env#env{vars=orddict:store(N, V, Vs)}.
+    Env#env{vars=?PUT(N, V, Vs)}.
 
 add_vbindings(Vbs, #env{vars=Vs0}=Env) ->
-    Vs1 = foldl(fun ({N,V}, Vs) -> orddict:store(N, V, Vs) end, Vs0, Vbs),
+    Vs1 = foldl(fun ({N,V}, Vs) -> ?PUT(N, V, Vs) end, Vs0, Vbs),
     Env#env{vars=Vs1}.
 
 is_vbound(N, #env{vars=Vs}) ->
-    orddict:is_key(N, Vs).
+    ?IS_KEY(N, Vs).
 
 get_vbinding(N, #env{vars=Vs}) ->
-    case orddict:find(N, Vs) of
+    case ?FIND(N, Vs) of
         {ok,V} -> {yes,V};
         error -> no
     end.
 
 fetch_vbinding(N, #env{vars=Vs}) ->
-    orddict:fetch(N, Vs).
+    ?GET(N, Vs).
 
 del_vbinding(N, #env{vars=Vs}=Env) ->
-    Env#env{vars=orddict:erase(N, Vs)}.
+    Env#env{vars=?ERASE(N, Vs)}.
 
 %% Functions.
 
@@ -130,7 +184,7 @@ add_fbinding_1(N, A, T, Fs) ->
                   {function,lists:keystore(A, 1, Fas, T)};
               (_) -> Def                        %Overwrite macros
           end,
-    orddict:update(N, Upd, Def, Fs).
+    ?UPDATE(N, Upd, Def, Fs).
 
 add_fbindings(Fbs, #env{funs=Fs0}=Env) ->
     Fs1 = foldl(fun ({N,A,V}, Fs) -> add_fbinding_1(N, A, {A,V}, Fs) end,
@@ -142,7 +196,7 @@ add_ibinding(M, R, A, L, #env{funs=Fs0}=Env) ->
     Env#env{funs=Fs1}.
 
 is_fbound(N, A, #env{funs=Fs}) ->
-    case orddict:find(N, Fs) of
+    case ?FIND(N, Fs) of
         {ok,{function,Fas}} ->
             case lists:keyfind(A, 1, Fas) of
                 false -> is_bif(N, A);
@@ -153,7 +207,7 @@ is_fbound(N, A, #env{funs=Fs}) ->
     end.
 
 get_fbinding(N, A, #env{funs=Fs}) ->
-    case orddict:find(N, Fs) of
+    case ?FIND(N, Fs) of
         {ok,{function,Fas}} ->
             case lists:keyfind(A, 1, Fas) of
                 {A,M,F} -> {yes,M,F};
@@ -177,7 +231,7 @@ get_bif(N, A) ->
     end.
 
 is_gbound(N, A, #env{funs=Fs}) ->
-    case orddict:find(N, Fs) of
+    case ?FIND(N, Fs) of
         {ok,{function,Fas}} ->
             case lists:keyfind(A, 1, Fas) of
                 false -> is_guard_bif(N, A);
@@ -188,7 +242,7 @@ is_gbound(N, A, #env{funs=Fs}) ->
     end.
 
 get_gbinding(N, A, #env{funs=Fs}) ->
-    case orddict:find(N, Fs) of
+    case ?FIND(N, Fs) of
         {ok,{function,Fas}} ->
             case lists:keyfind(A, 1, Fas) of
                 false -> get_guard_bif(N, A);
@@ -207,21 +261,21 @@ get_guard_bif(N, A) ->
 %% Macros.
 
 add_mbinding(N, V, #env{funs=Fs}=Env) ->
-    Env#env{funs=orddict:store(N, {macro,V}, Fs)}.
+    Env#env{funs=?PUT(N, {macro,V}, Fs)}.
 
 add_mbindings(Fbs, #env{funs=Fs0}=Env) ->
-    Fs1 = foldl(fun ({N,V}, Fs) -> orddict:store(N, {macro,V}, Fs) end,
+    Fs1 = foldl(fun ({N,V}, Fs) -> ?PUT(N, {macro,V}, Fs) end,
                 Fs0, Fbs),
     Env#env{funs=Fs1}.
 
 is_mbound(N, #env{funs=Fs}) ->
-    case orddict:find(N, Fs) of
+    case ?FIND(N, Fs) of
         {ok,{macro,_}} -> true;
         _ -> false
     end.
 
 get_mbinding(N, #env{funs=Fs}) ->
-    case orddict:find(N, Fs) of
+    case ?FIND(N, Fs) of
         {ok,{macro,V}} -> {yes,V};
         _ -> no
     end.
