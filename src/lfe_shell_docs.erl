4c9f2ede47f710dfb0548d4e32793ad5766ca8fa
Robert Virding <rvirding@gmail.com>
2022-01-29 17:26:21 +0100

    Improve rendering of module documentation fron the REPL
    Also took the chance to remove import of lfe_env functions and
    explicitly call them in the lfe_module. We will do this with other
    modules as well when we work with them.

--- src/lfe_shell_docs.erl
+++ src/lfe_shell_docs.erl
@@ -1,4 +1,4 @@
-%% Copyright (c) 2021 Robert Virding
+%% Copyright (c) 2022 Robert Virding
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -30,6 +30,7 @@
 -define(GRN(Str), "\e[1;32m" ++ Str ++ "\e[0m").
 -define(YLW(Str), "\e[1;33m" ++ Str ++ "\e[0m").
 -define(BLU(Str), "\e[1;34m" ++ Str ++ "\e[0m").
+-define(BOLD(Str), "\e[1m" ++ Str ++ "\e[0m").
 
 %% render(Module, Docs) -> unicode:chardata().
 
@@ -37,33 +38,54 @@ render(Bin, Docs) when is_binary(Bin) ->
     {ok,{Mod,_}} = beam_lib:chunks(Bin, [], []), %Sneaky!
     render(Mod, Docs);
 render(Mod, #docs_v1{format = ?LFE_FORMAT, module_doc=Mdoc}) ->
-    [lfe_io:format1(?BLU("~p")++"\n\n", [Mod]), return_doc(Mdoc)].
+    [red_line(60),
+     lfe_io:format1(?BLU("~p")++"\n\n", [Mod]),
+     return_doc(Mod, Mdoc)].
 
 %% render(Module, Function, Docs) -> unicode:chardata().
 
 render(_Mod, Name, #docs_v1{format = ?LFE_FORMAT, docs = Docs}) ->
-    Fns = [ F || {{_,N,_},_,_,_,_}=F <- Docs, N =:= Name ],
-    Ret = lists:map(fun ({{function,_,_},_,Sig,Doc,_}) ->
-			    [lfe_io:format1(?BLU("defun ~s")++"\n\n", [Sig]),
-			     return_doc(Doc)];
-			({{macro,_,_},_,Sig,Doc,_}) ->
-			    [lfe_io:format1(?BLU("defmacro ~s")++"\n\n", [Sig]),
-			     return_doc(Doc)]
-		    end, Fns),
+    Render = fun ({{function,_Func,_Ar},_,Sig,Doc,Meta}) ->
+                     [red_line(60),
+                      return_sig(function, Sig, Meta),
+                      return_doc(Sig, Doc)];
+                 ({{macro,_Macro,_},_,Sig,Doc,Meta}) ->
+                     [red_line(60),
+                      return_sig(macro, Sig, Meta),
+                      return_doc(Sig, Doc)]
+             end,
+    Ret = [ Render(F) || {{_,N,_},_,_,_,_}=F <- Docs, N =:= Name ],
     return_render(Ret, function_missing).
 
 %% render(Module, Function, Arity, Docs) -> unicode:chardata().
 
 render(_Mod, Name, Arity, #docs_v1{format = ?LFE_FORMAT, docs = Docs}) ->
-    Fns = [ F || {{function,N,A},_,_,_,_}=F <- Docs, N =:= Name, A =:= Arity ],
-    Ret = lists:map(fun ({{function,_,_},_,Sig,Doc,_}) ->
-			    [lfe_io:format1(?BLU("defun ~s")++"\n\n", [Sig]),
-			     return_doc(Doc)]
-		    end, Fns),
+    Render = fun ({{function,_Func,_Ar},_,Sig,Doc,Meta}) ->
+                     [red_line(60),
+                      return_sig(function, Sig, Meta),
+                      return_doc(Sig, Doc)]
+             end,
+    Ret = [ Render(F) || {{function,N,A},_,_,_,_}=F <- Docs,
+                         N =:= Name, A =:= Arity ],
     return_render(Ret, function_missing).
 
-return_doc(#{<<"en">> := Dv}) -> lfe_io:format1("~s\n\n", [Dv]);
-return_doc(_) -> "\n".
+return_doc(_Missing, #{<<"en">> := Dv}) ->
+    lfe_io:format1("~s\n", [Dv]);
+return_doc(Missing, None) when None =:= none; None =:= #{} ->
+    lfe_io:format1(<<"No documentation for ~s\n">>, [Missing]);
+return_doc(Missing, _Docs) ->
+    lfe_io:format1(<<"Unknown format for ~s\n">>, [Missing]).
+
+%% return_sig(_Type, _Sig, #{signature:=[Spec]}) ->
+%%     lfe_io:format1(?BLU("~s") ++ "\n", [erl_pp:form(Spec)]);
+return_sig(Type, Sig, _Meta) ->
+    lfe_io:format1(?BLU("~s ~s") ++ "\n\n", [Type,Sig]).
 
 return_render([], Error) -> {error,Error};
 return_render(FDocs, _Error) -> FDocs.
+
+%% red_line(Length) -> ok.
+%%  Output a red line of Length characters.
+
+red_line(Len) ->
+    io_lib:format(?RED("~*c")++"\n", [Len,$-]).
